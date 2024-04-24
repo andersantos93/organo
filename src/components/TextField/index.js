92b7c7d27c73fbd9ff50971614ea8f006f67ef89
Anderson Santos <anderson.chenko1993@gmail.com>
2024-04-24 00:20:28 -0300

    feat: add organo form

---
+++ src/components/TextField/index.js
@@ -0,0 +1,13 @@
+import "./TextField.css"
+
+const TextField = (props) => {
+    const placeholderModified = `${props.placeholder}...`;
+    return (
+        <div className="field-text">
+            <label>{props.label}</label>
+            <input placeholder={placeholderModified} />
+        </div>
+    );
+}
+
+export default TextField;
